c86f9c83b7966311f95e118623e733716a711f69
shaarifalam <alamshaarif@gmail.com>
2024-01-17 21:33:49 +0530

    Added basic functions

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1. For Using Strict mode - for not to encounter silly errors
+"use strict";
+
+// 2. Use let for creating variables instead of var - for not to encounter undefined errors
+let newVariable = "Shaarif Alam";
+console.log(newVariable);
+
+// 3. Const is a constant value whose value cant be changed
+const myName = "Shaarif";
+console.log(myName);
+
+// 4. Type of operator can be used to inspect the type of values
+console.log(typeof myName);
+
+// 5. Undefined variables can be set
+let myName2;
+console.log(typeof myName2);
+
+// 6. Convert String to Number
+var score = 33;
+console.log(typeof score);
+// output is number
+var score = String(33);
+console.log(typeof score);
+// output is String now
+
+// 7. Assigning negative valuess
+let value = 3;
+let negValue = -value;
+console.log(negValue);
